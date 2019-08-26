81147e16cce30fac6ac9add82bd17fb2b075fa3e
scooby <satish.pillai888@gmail.com>
2019-08-26 10:41:54 +0530

    test

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -24,6 +24,6 @@ const appStack = createSwitchNavigator({
 } 
 )
 
-const appContainer = createAppContainer(appStack) 
+const appContainer = createAppContainer(appStack)  
 
 export default appContainer;
